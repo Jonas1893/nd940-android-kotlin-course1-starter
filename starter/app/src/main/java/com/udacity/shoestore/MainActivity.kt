01c31da080b1293034df128f460b14fc979be986
Jonas Reichert <jonas.reichert@daimler.com>
2021-03-05 14:22:21 +0100

    wip

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,6 +2,8 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
@@ -23,7 +25,14 @@ class MainActivity : AppCompatActivity() {
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setupNavigation(binding)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu);
+
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.logout_menu, menu)
+        return true
     }
 
     private fun setupNavigation(binding: ActivityMainBinding) {
@@ -31,6 +40,6 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
         NavigationUI.setupWithNavController(binding.toolbar, navController)
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 }
