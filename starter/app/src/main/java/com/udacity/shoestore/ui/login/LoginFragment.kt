dfcc94cd6d6629b10dc1c88e07e2c5c3c877c72c
Jonas Reichert <jonas.reichert@daimler.com>
2021-03-05 17:00:15 +0100

    implemented navigate to welcome

--- starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -15,8 +15,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
@@ -25,10 +28,28 @@ class LoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false)
+
+        setupButtonOnClickListeners(binding)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupButtonOnClickListeners(binding: FragmentLoginBinding) {
+        binding.signinButton.setOnClickListener { view: View ->
+            navigateToWelcomeScreen(view)
+        }
+
+        binding.registerButton.setOnClickListener { view: View ->
+            navigateToWelcomeScreen(view)
+        }
+    }
+
+    private fun navigateToWelcomeScreen(view: View) {
+        view.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment3)
     }
 }
