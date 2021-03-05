2882b90f94e45d1535be518de24d4b54fa31cf44
Jonas Reichert <jonas.reichert@daimler.com>
2021-03-05 09:18:41 +0100

    adding navigation destinations

---
+++ starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -0,0 +1,36 @@
+package com.udacity.shoestore.ui.login
+
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+
+import com.udacity.shoestore.R
+
+class LoginFragment : Fragment() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginViewModel.kt
@@ -0,0 +1,11 @@
+package com.udacity.shoestore.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+
+import com.udacity.shoestore.R
+
+class LoginViewModel : ViewModel() {
+}
