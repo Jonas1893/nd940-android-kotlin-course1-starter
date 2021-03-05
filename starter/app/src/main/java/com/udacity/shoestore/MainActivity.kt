2c5ad9c9e0e3c71ce827eadcfef744d71f7ceac5
Jonas Reichert <jonas.reichert@daimler.com>
2021-03-05 13:39:28 +0100

    wip

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,35 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.circularreveal.coordinatorlayout.CircularRevealCoordinatorLayout
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         Timber.plant(Timber.DebugTree())
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setupNavigation(binding)
+
+    }
+
+    private fun setupNavigation(binding: ActivityMainBinding) {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        NavigationUI.setupWithNavController(binding.toolbar, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 }
