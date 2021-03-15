ff65ea1b7e72fce48782d1bfbef76d3e614c3ad7
Jonas Reichert <jonas.reichert@daimler.com>
2021-03-15 11:08:40 +0100

    working setup with viewModel

--- starter/app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListViewModel.kt
@@ -1,5 +1,6 @@
 package com.udacity.shoestore.ui.shoelist
 
+import android.media.Image
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,4 +12,21 @@ class ShoeListViewModel : ViewModel() {
     val shoes: LiveData<List<Shoe>>
         get() = _shoes
 
+    init {
+        var shoeList = ArrayList<Shoe>(0)
+        val shoe1Images = ArrayList<Image>(0)
+
+        val shoe1 = Shoe("Air",
+            42.0,
+            "Nike",
+            "Die Nike Air Technologie besteht aus komprimierter Luft in einer robusten " +
+                    "und doch flexiblen Kammer. Sie bietet mehr Flexibilität und Sprungkraft, " +
+                    "ohne dabei auf Halt zu verzichten. Die Air-Sole-Elemente behalten dank ihrer " +
+                    "Elastizität ihre Form und reduzieren Stöße. Der Schuh bleibt leicht " +
+                    "und umschließt den Fuß.")
+
+        shoeList.add(shoe1)
+
+        _shoes.value = shoeList
+    }
 }
