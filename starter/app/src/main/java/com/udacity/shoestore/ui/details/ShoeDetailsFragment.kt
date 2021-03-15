0943dec572e25ec6f5abc39d0024d0a91675b3bc
Jonas Reichert <jonas.reichert@daimler.com>
2021-03-15 16:17:03 +0100

    inject viewModels

--- starter/app/src/main/java/com/udacity/shoestore/ui/details/ShoeDetailsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/details/ShoeDetailsFragment.kt
@@ -2,6 +2,7 @@ package com.udacity.shoestore.ui.details
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,8 +10,11 @@ import androidx.databinding.DataBindingUtil
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailsBinding
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.ui.shoelist.ShoeListViewModel
 
 class ShoeDetailsFragment : Fragment() {
+
+    private val viewModel: ShoeListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
